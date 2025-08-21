01116e7337d45f0157be00e344e3d86e592e033a
jbelanger <jbelanger@users.noreply.github.com>
2025-08-20 20:02:44 -0400

    Manual registration of providers

---
+++ packages/import/src/blockchains/registry/register-providers.ts
@@ -0,0 +1,27 @@
+// Import all blockchain providers to trigger their registration
+// This must be imported before using the ProviderRegistry
+
+// Bitcoin Providers
+import '../bitcoin/providers/BlockCypherProvider.ts';
+import '../bitcoin/providers/BlockstreamProvider.ts';
+import '../bitcoin/providers/MempoolSpaceProvider.ts';
+
+// Ethereum Providers
+import '../ethereum/providers/AlchemyProvider.ts';
+import '../ethereum/providers/EtherscanProvider.ts';
+import '../ethereum/providers/MoralisProvider.ts';
+
+// Solana Providers
+import '../solana/providers/HeliusProvider.ts';
+import '../solana/providers/SolanaRPCProvider.ts';
+import '../solana/providers/SolscanProvider.ts';
+
+// Avalanche Providers
+import '../avalanche/providers/SnowtraceProvider.ts';
+
+// Injective Providers
+import '../injective/providers/InjectiveExplorerProvider.ts';
+import '../injective/providers/InjectiveLCDProvider.ts';
+
+// Polkadot/Substrate Providers
+import '../polkadot/providers/SubstrateProvider.ts';
