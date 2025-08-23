f0fcebdc75df2a77a78d12769020ef39a52bebf7
jbelanger <joblo10@hotmail.com>
2025-08-22 23:38:24 -0400

    refactor: Move Avalanche address validation to utils; remove unused functions from types; implement transaction type parsing for Substrate in utils; clean up Solana transaction type parsing

--- packages/import/src/blockchains/polkadot/utils.ts
+++ packages/import/src/blockchains/polkadot/utils.ts
@@ -18,3 +18,35 @@ export function encodeAddress(
   // For now, return placeholder - implement with proper SS58 encoding library
   throw new Error("SS58 encoding not implemented - use @polkadot/util-crypto");
 }
+
+// Parse Substrate method to transaction type
+export function parseSubstrateTransactionType(
+  module: string,
+  method: string,
+): "transfer" | "transfer_keep_alive" | "force_transfer" | "staking_bond" | "staking_unbond" | "staking_withdraw_unbonded" | "staking_nominate" | "staking_chill" | "utility_batch" | "custom" {
+  const key = `${module}_${method}`.toLowerCase();
+
+  switch (key) {
+    case "balances_transfer":
+    case "balances_transfer_all":
+      return "transfer";
+    case "balances_transfer_keep_alive":
+      return "transfer_keep_alive";
+    case "balances_force_transfer":
+      return "force_transfer";
+    case "staking_bond":
+      return "staking_bond";
+    case "staking_unbond":
+      return "staking_unbond";
+    case "staking_withdraw_unbonded":
+      return "staking_withdraw_unbonded";
+    case "staking_nominate":
+      return "staking_nominate";
+    case "staking_chill":
+      return "staking_chill";
+    case "utility_batch":
+      return "utility_batch";
+    default:
+      return "custom";
+  }
+}
