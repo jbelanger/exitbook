abc80a6aa28824a325356769a562412ab40a5693
jbelanger <your-username@users.noreply.github.com>
2025-11-03 14:36:08 -0500

    refactor(database): remove database clearing option from initialization #153

--- packages/platform/data/src/storage/initialization.ts
+++ packages/platform/data/src/storage/initialization.ts
@@ -1,6 +1,6 @@
 import { getLogger } from '@exitbook/shared-logger';
 
-import { clearDatabase, createDatabase, type KyselyDB } from './database.js';
+import { createDatabase, type KyselyDB } from './database.js';
 import { runMigrations } from './migrations.js';
 
 const logger = getLogger('DatabaseInitialization');
@@ -8,15 +8,10 @@ const logger = getLogger('DatabaseInitialization');
 /**
  * Initialize database with migrations
  */
-export async function initializeDatabase(shouldClearDatabase = false): Promise<KyselyDB> {
+export async function initializeDatabase(): Promise<KyselyDB> {
   logger.info('Initializing database...');
 
   const database = createDatabase();
-
-  if (shouldClearDatabase) {
-    await clearDatabase(database);
-    logger.info('Database cleared and reinitialized');
-  }
 
   // Run migrations to ensure schema is up to date
   await runMigrations(database);
